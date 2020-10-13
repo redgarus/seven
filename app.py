f5b8e5681bc9e8c21a128412ecc369da7afa6aca
Nega-Tiv <62760206+Nega-Tiv@users.noreply.github.com>
2020-10-13 21:22:20 +0300

    Add all files

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+
+@app.route('/')
+def redirected():
+    return redirect("/home")
+
+
+@app.route('/home')
+def index():
+    return render_template("main.html")
+
+
+@app.route('/students')
+def pupils():
+    return render_template("students.html")
+
+
+@app.route('/teachers')
+def teacher():
+    return render_template("teachers.html")
+
+
+@app.route('/dnevnik')
+def dnevnik():
+    return render_template("dnevnik.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
