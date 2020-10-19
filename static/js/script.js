4facda979dd8fe5c4b5a80af36fed5e8997ca035
Nega-Tiv <62760206+Nega-Tiv@users.noreply.github.com>
2020-10-19 22:49:43 +0300

    Make

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+function isMobile() { 
+
+return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
+}
+
+if(isMobile()){
+    header("Location: https://www.google.com");
+}
+else { alert("Marat") }
